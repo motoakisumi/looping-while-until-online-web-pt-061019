73c9a77cf3923de00e15d5de6864ba7a595cbde2
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-15 10:16:53 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,7 @@ def using_until
   levitation_force = 6
   
   until levitation_force == 10
-    puts "Levitation is #{levitation_force}"
+    puts "Wingardium Leviosa"
     levitation += 1
   end
     
