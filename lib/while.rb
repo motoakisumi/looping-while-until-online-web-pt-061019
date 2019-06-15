9282b398c27b4185a4d36aecb3b686fdd0385d13
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-15 10:14:43 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,7 @@
 def using_while
   levitation_force = 6
-	puts "Levitation is #{levitation_force}"
+  while levitation_force < 10
+  puts "Levitation is #{levitation_force}"
   levitation_force +=1
 end
 
